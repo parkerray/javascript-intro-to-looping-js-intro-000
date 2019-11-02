d2b675895a2467c7b7cf189371ea784ca254cf77
Parker Thompson <park.thompson@gmail.com>
2019-11-02 15:15:22 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,17 @@
+function forLoop(arr) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      arr.push(`I am ${i} strange loop.`)
+    } else {
+        arr.push(`I am ${i} strange loops.`)
+    }
+  }
+  return arr;
+}
+
+function whileLoop(number) {
+  while (number > -1) {
+    console.log(number)
+  }
+  return 'done'
+}
